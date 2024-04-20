7b92699d391652dfcb1f1855df8f8fff24b10a67
Burak Can <burak@burakcan.me>
2024-04-20 11:44:19 +0300

    fix scrolls

---
+++ src/components/CraftBuilder/CraftBuilderTopBar/Avatar.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useOrganization, useUser } from "@clerk/nextjs";
+import Image from "next/image";
+
+export function Avatar() {
+  const { organization, isLoaded: orgLoaded } = useOrganization();
+  const { user, isLoaded: userLoaded } = useUser();
+
+  const avatar = organization?.imageUrl || user?.imageUrl;
+
+  return (
+    <div className="border p-1 rounded-lg">
+      <div
+        className="size-8 relative rounded-md"
+        style={{
+          background: "rgba(0, 0, 0, 0.24)",
+        }}
+      >
+        {orgLoaded && userLoaded && (
+          <Image
+            src={avatar!}
+            alt="avatar"
+            className="rounded-md"
+            unoptimized
+            style={{
+              objectFit: "cover",
+            }}
+            fill
+          />
+        )}
+      </div>
+    </div>
+  );
+}
--- src/components/CraftBuilder/CraftBuilderTopBar/CraftBuilderTopBar.tsx
+++ src/components/CraftBuilder/CraftBuilderTopBar/CraftBuilderTopBar.tsx
@@ -1,85 +1,54 @@
-import { auth, currentUser } from "@clerk/nextjs/server";
 import { ArrowLeft, EyeIcon, LinkIcon } from "lucide-react";
 import Link from "next/link";
+import { Avatar } from "./Avatar";
 import { CraftName } from "./CraftName";
 import { SaveAndPublishButton } from "./SaveAndPublishButton";
 import { SaveButton } from "./SaveButton";
 import { TopBar } from "@/components/AppChrome";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 interface Props {
   craft_id: string;
   activeTab: "create" | "flow" | "connect" | "share" | "results";
 }
 
-export async function CraftBuilderTopBar(props: Props) {
+export function CraftBuilderTopBar(props: Props) {
   const { craft_id, activeTab } = props;
 
-  let workspaceName: string | undefined | null = null;
-
-  const { sessionClaims } = auth();
-
-  if (sessionClaims?.org_name) {
-    workspaceName = sessionClaims.org_name as string;
-  } else {
-    const user = await currentUser();
-    workspaceName = user?.firstName;
-  }
-
   return (
-    <TopBar className="flex items-center">
-      <Link
-        href="/dashboard"
-        className="h-full w-16 flex items-center justify-center"
-      >
-        <ArrowLeft className="size-5" />
-      </Link>
-      <Breadcrumb>
-        <BreadcrumbList className="text-sm">
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <Link href="/dashboard">
-                {workspaceName === null
-                  ? "Dashboard"
-                  : `${workspaceName}'s Workspace`}
-              </Link>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <CraftName />
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
-      <div className="flex-1" />
-      <Tabs defaultValue={activeTab}>
-        <TabsList>
-          <TabsTrigger value="create" asChild>
-            <Link href={`/form/${craft_id}/edit`}>Create</Link>
-          </TabsTrigger>
-          <TabsTrigger value="flow" asChild>
-            <Link href={`/form/${craft_id}/flow`}>Flow</Link>
-          </TabsTrigger>
-          <TabsTrigger value="connect" asChild>
-            <Link href={`/form/${craft_id}/edit/connect`}>Connect</Link>
-          </TabsTrigger>
-          <TabsTrigger value="share" asChild>
-            <Link href={`/form/${craft_id}/edit/share`}>Share</Link>
-          </TabsTrigger>
-          <TabsTrigger value="results" asChild>
-            <Link href={`/form/${craft_id}/edit/results`}>Results</Link>
-          </TabsTrigger>
-        </TabsList>
-      </Tabs>
-      <div className="flex-1" />
-      <div className="flex gap-2 pr-4">
+    <TopBar className="grid grid-cols-3 items-center">
+      <div className="flex h-full items-center gap-2">
+        <Link href="/dashboard" className="h-full flex items-center">
+          <div className="w-12 flex items-center justify-center">
+            <ArrowLeft className="size-5" />
+          </div>
+          <Avatar />
+        </Link>
+        <CraftName />
+      </div>
+      <div className="">
+        <Tabs defaultValue={activeTab}>
+          <TabsList>
+            <TabsTrigger value="create" asChild>
+              <Link href={`/form/${craft_id}/edit`}>Create</Link>
+            </TabsTrigger>
+            <TabsTrigger value="flow" asChild>
+              <Link href={`/form/${craft_id}/flow`}>Flow</Link>
+            </TabsTrigger>
+            <TabsTrigger value="connect" asChild>
+              <Link href={`/form/${craft_id}/edit/connect`}>Connect</Link>
+            </TabsTrigger>
+            <TabsTrigger value="share" asChild>
+              <Link href={`/form/${craft_id}/edit/share`}>Share</Link>
+            </TabsTrigger>
+            <TabsTrigger value="results" asChild>
+              <Link href={`/form/${craft_id}/edit/results`}>Results</Link>
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
+      </div>
+      <div className="flex gap-2 pr-4 justify-end">
         <Button size="icon" variant="outline">
           <LinkIcon className="size-4" />
         </Button>
