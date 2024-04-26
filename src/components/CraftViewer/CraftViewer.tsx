3ef69a558c814eb54fbf8c3a91fa174e2d916841
Burak Can <burak@burakcan.me>
2024-04-26 16:40:23 +0300

    page loading

--- src/components/CraftViewer/CraftViewer.tsx
+++ src/components/CraftViewer/CraftViewer.tsx
@@ -5,9 +5,11 @@ import type { CraftVersion } from "@prisma/client";
 import { useMemo, useState } from "react";
 import { PageLayout } from "../CraftBuilder/PageRenderer/PageLayout";
 import { ThemeStyle } from "../CraftBuilder/PageRenderer/ThemeStyle";
+import { FullPageLoading } from "../FullPageLoading";
 import { craftPageDefinitions } from "@/craftPages";
 import { MadeWithFormCraftViewer } from "@/craftPages/atoms/MadeWithFormcraft";
 import type { CraftTheme } from "@/craftPages/schemas/theming";
+import { useLoadPageResources } from "@/hooks/useLoadPageResources";
 
 interface Props {
   version: CraftVersion;
@@ -27,17 +29,19 @@ export function CraftViewer(props: Props) {
     return version.data.pages.find(
       (page) => page.id === currentNode?.data.pageId
     );
-  }, [currentNode, version.data.pages]);
-
-  if (!currentNode || !currentPage) {
-    return null;
-  }
+  }, [currentNode, version.data.pages]) as FormCraft.CraftPage;
 
   const currentPageBaseTheme = themes[currentPage.baseThemeId];
   const theme = {
     ...currentPageBaseTheme,
     ...currentPage.themeOverride,
   };
+
+  const { fontsLoaded, imagesLoaded } = useLoadPageResources(
+    currentPage,
+    theme
+  );
+
   const pageDefinition = craftPageDefinitions[currentPage.type];
 
   return (
@@ -55,6 +59,7 @@ export function CraftViewer(props: Props) {
         prose-headings:font-bold
   `}
     >
+      <FullPageLoading visible={!fontsLoaded || !imagesLoaded} />
       <link
         href={`https://fonts.googleapis.com/css2?family=${theme.titleFont}&display=swap`}
         rel="stylesheet"
