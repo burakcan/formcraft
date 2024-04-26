9c4911043c4ecc858a6acaa50062ccee7bc001e7
Burak Can <burak@burakcan.me>
2024-04-26 15:19:15 +0300

    delete old code

--- src/lib/__craftPageConfig/statement.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-import { basePage } from "./basePage";
-
-export const statement = basePage.extend({
-  type: z.literal("statement").default("statement"),
-  cta: z.string().default("Let's go!"),
-});
-
-export type Statement = z.infer<typeof statement>;
