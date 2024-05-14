0dcddaa80bbd4021ba808a6e80c9f23fc554536b
Burak Can <burak@burakcan.me>
2024-05-14 11:14:54 +0200

    initial paddle integration

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -1,4 +1,6 @@
 import type { WebhookEvent } from "@clerk/nextjs/server";
+import type { Environment } from "@paddle/paddle-node-sdk";
+import { Paddle } from "@paddle/paddle-node-sdk";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
@@ -37,28 +39,92 @@ export async function POST(req: NextRequest) {
     const event = await verifyEvent(req);
 
     if (relevantEvents.has(event.type)) {
+      const paddle = new Paddle(process.env.PADDLE_API_KEY as string, {
+        environment: process.env.NEXT_PUBLIC_PADDLE_ENV as Environment,
+      });
+
       switch (event.type) {
         case "user.created":
-        case "user.updated":
-          await db.user.upsert({
-            where: { id: event.data.id },
-            update: { id: event.data.id },
-            create: { id: event.data.id },
+        case "user.updated": {
+          await db.$transaction(async (tx) => {
+            const user = await tx.user.upsert({
+              where: { id: event.data.id },
+              create: { id: event.data.id },
+              update: { id: event.data.id },
+            });
+
+            const primaryEmail = event.data.email_addresses.find(
+              (e) => e.id === event.data.primary_email_address_id
+            );
+
+            if (!primaryEmail) {
+              return;
+            }
+
+            if (user.paddleCustomerId) {
+              await paddle.customers.update(user.paddleCustomerId, {
+                email: primaryEmail.email_address,
+              });
+            } else {
+              const paddleCustomer = await paddle.customers.create({
+                email: primaryEmail.email_address,
+              });
+
+              await tx.user.update({
+                where: { id: event.data.id },
+                data: {
+                  paddleCustomerId: paddleCustomer.id,
+                },
+              });
+            }
           });
           break;
+        }
+
         case "user.deleted":
           await db.user.delete({
             where: { id: event.data.id },
           });
           break;
         case "organization.created":
-        case "organization.updated":
-          await db.organization.upsert({
-            where: { id: event.data.id },
-            update: { id: event.data.id },
-            create: { id: event.data.id },
+        case "organization.updated": {
+          await db.$transaction(async (tx) => {
+            const user = await tx.user.findUniqueOrThrow({
+              where: { id: event.data.created_by },
+            });
+
+            if (!user.paddleCustomerId) {
+              throw new Error("User does not have a customer id");
+            }
+
+            const organization = await tx.organization.upsert({
+              where: { id: event.data.id },
+              update: { id: event.data.id },
+              create: { id: event.data.id },
+            });
+
+            if (organization.paddleBusinessId) {
+              await paddle.businesses.update(
+                user.paddleCustomerId,
+                organization.paddleBusinessId,
+                { name: event.data.name }
+              );
+            } else {
+              const paddleBusiness = await paddle.businesses.create(
+                user.paddleCustomerId,
+                { name: event.data.name }
+              );
+
+              await tx.organization.update({
+                where: { id: event.data.id },
+                data: {
+                  paddleBusinessId: paddleBusiness.id,
+                },
+              });
+            }
           });
           break;
+        }
         case "organization.deleted":
           await db.organization.delete({
             where: { id: event.data.id },
