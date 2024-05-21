345e98b4ba5e3d900b6ea0b06cc17df547edca7f
Burak Can <burak@burakcan.me>
2024-05-21 11:33:55 +0200

    compose

--- src/services/db/index.ts
+++ src/services/db/index.ts
@@ -1,7 +1,11 @@
 import "server-only";
 import { PrismaClient } from "@prisma/client";
+import { PHASE_PRODUCTION_BUILD } from "next/constants";
 
 const prismaClientSingleton = () => {
+  if (process.env.NEXT_PHASE === PHASE_PRODUCTION_BUILD) {
+    return {} as PrismaClient;
+  }
   console.log("Creating new PrismaClient!");
   return new PrismaClient();
 };
