9446045325347d580fcd65ad254dc47f430772ed
Burak Can <burak@burakcan.me>
2024-05-21 17:47:58 +0200

    fix linter

--- src/craftPages/StripePayment/ContentSettings.tsx
+++ src/craftPages/StripePayment/ContentSettings.tsx
@@ -1,6 +1,5 @@
 import { InputGroup } from "../contentAtoms/InputGroup";
 import { Logo } from "../contentAtoms/Logo";
-import { OptionalInputField } from "../contentAtoms/OptionalInputField";
 import { SettingsWrapper } from "../contentAtoms/SettingsWrapper";
 import { SwitchField } from "../contentAtoms/SwitchField";
 import { VariableName } from "../contentAtoms/VariableName";
@@ -22,15 +21,6 @@ export function StripePaymentContentSettings(props: Props) {
           name="required"
           checked={page.required}
           onCheckedChange={(value) => onChange({ ...page, required: value })}
-        />
-        <OptionalInputField
-          label="Max characters"
-          name="maxLength"
-          placeHolder="1 - 999999999"
-          type="number"
-          value={page.maxLength}
-          onChange={(maxLength) => onChange({ ...page, maxLength })}
-          format={Number}
         />
       </InputGroup>
       <VariableName page={page} onChange={onChange} />
