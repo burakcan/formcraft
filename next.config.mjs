862da947f5dff3e618f01cd533a49d888c9bf7a2
Burak Can <burak@burakcan.me>
2024-05-21 11:55:19 +0200

    no standalone

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   images: {
     domains: ['plus.unsplash.com', 'images.unsplash.com', 'imagedelivery.net'],
   },
