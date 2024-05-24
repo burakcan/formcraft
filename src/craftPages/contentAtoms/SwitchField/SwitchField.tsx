b194d5a9b0af1c3f10776902b9553a81836471af
Burak Can <burak@burakcan.me>
2024-05-24 17:32:45 +0200

    payment preview

--- src/craftPages/contentAtoms/SwitchField/SwitchField.tsx
+++ src/craftPages/contentAtoms/SwitchField/SwitchField.tsx
@@ -7,16 +7,18 @@ interface Props {
   name: string;
   checked: boolean;
   onCheckedChange: (checked: boolean) => void;
+  disabled?: boolean;
 }
 
 export function SwitchField(props: Props) {
-  const { label, name, checked, onCheckedChange } = props;
+  const { label, name, checked, onCheckedChange, disabled } = props;
 
   return (
     <div className="flex items-center justify-between">
       <Label htmlFor={name}>{label}</Label>
       <SmallSwitch
         name={name}
+        disabled={disabled}
         checked={checked}
         onCheckedChange={onCheckedChange}
       />
