b6a0aa9692285996486965c58682ad732a7c988d
Burak Can <burak@burakcan.me>
2024-06-10 18:32:13 +0200

    stripe confetti

--- src/app/(app)/(authenticated)/stripe-connected/page.tsx
+++ src/app/(app)/(authenticated)/stripe-connected/page.tsx
@@ -1,12 +1,32 @@
-import { FaStripeS } from "react-icons/fa";
-import Confetti from "@/components/Confetti";
+"use client";
 
-export default async function StripeConnected() {
+import { useLayoutEffect } from "react";
+import { FaStripe } from "react-icons/fa";
+
+export default function StripeConnected() {
+  useLayoutEffect(() => {
+    import("@tsparticles/confetti").then(({ confetti }) => {
+      confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 },
+        shapes: ["emoji"],
+        shapeOptions: {
+          emoji: {
+            value: ["💵", "💶", "💷", "🎉"],
+          },
+        },
+      });
+    });
+  }, []);
+
   return (
     <div className="fixed top-0 left-0 size-full flex items-center justify-center bg-accent">
-      <Confetti />
       <div className="p-4 bg-background rounded shadow-md">
-        <FaStripeS className="bg-indigo-500" size={48} />
+        <FaStripe
+          className="bg-indigo-500 text-white p-2 rounded mb-2"
+          size={48}
+        />
         <h1 className="text-2xl font-bold">Success!</h1>
         <p className="mt-2">
           Your Stripe account has been successfully connected.
